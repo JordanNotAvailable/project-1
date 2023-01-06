9c0ddd70d7bb35e6d7185dfbfb4ce379bf37c533
jasonyoo3026 <jasonyoo3026@gmail.com>
2023-01-06 21:20:15 +1100

    adding js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function getrecepe(query){
+    $.ajax({
+        url:"https://api.spoonacular.com/recipes/search?apiKey=d03a24375b30427a863ee13274b2ca29&number=1&query="+query,
+        success: function(res){
+            document.getElementById("output").innerHTML = "<h1>" + res.results[0].title + "</h1><br><img src='" + res.baseUri + res.results[0].image + "' width='400'/><br> ready in " + res.results[0].readyInMinutes + " minutes" + "<br><a href='" + res.results[0].sourceUrl +"'>Link to recipe</a>";
+        }
+    })
+}
